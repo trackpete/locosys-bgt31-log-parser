84643822259cd0c4d212813f6faa458e00229894
Pete Waterman <pete.waterman@it.com>
2018-10-03 10:20:55 -0400

    Minor imporvements to detecting filename dates

--- parse.js
+++ parse.js
@@ -29,16 +29,19 @@ recurse(dataDir).then(
 // Function to scan the GPS file
 function scanFile(gpsFile, callback) {
   console.log("Scanning data file:", gpsFile);
-  // Instead of using a third party parser, we're going to do some of our own magic
-  // Read in the file, find the specific lines we want, split the CSV, and add
-  // them to an array
-  const gpsData = {};
 
-  // NOTE: Looks like the timestamp in the NMEA sentence is 24 hour only.
-  // I'll need to add parsing of date from filename
-  // hopefully I didn't change any filenames in my records...
-
-  lineReadFile = new lineByLineReader(gpsFile)
+  // Only parse actual GPSUSER text files that we can extract the date from
+  if (fileName = gpsFile.match(/\/GPSUSER_\d+_(\d+)_\d+\.TXT/)) {
+    // Instead of using a third party parser, we're going to do some of our own magic
+    // Read in the file, find the specific lines we want, split the CSV, and add
+    // them to an array
+    const gpsData = {};
+  
+    // NOTE: Looks like the timestamp in the NMEA sentence is 24 hour only.
+    // I'll need to add parsing of date from filename
+    // hopefully I didn't change any filenames in my records...
+  
+    lineReadFile = new lineByLineReader(gpsFile)
     .on('error', function (err) {
       if(err) throw err;
     })
@@ -65,4 +68,7 @@ function scanFile(gpsFile, callback) {
       console.log("Done reading", gpsFile);
       console.log(JSON.stringify(gpsData, null, 2));
     });
+  } else {
+    console.log("Ignoring file", gpsFile, "as it does not appear to be a standard named locosys file");
+  }
 }
